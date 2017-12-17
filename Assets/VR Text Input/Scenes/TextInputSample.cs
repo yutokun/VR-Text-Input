b6b6e6bc956f0e92af136a97115e9d74d3dec5e1
yutoVR <yuto.fushimi@icloud.com>
2017-12-17 16:09:22 +0900

    Added result selection to the sample script.

--- Assets/VR Text Input/Scenes/TextInputSample.cs
+++ Assets/VR Text Input/Scenes/TextInputSample.cs
@@ -5,20 +5,22 @@ using UnityEngine.SceneManagement;
 public class TextInputSample : MonoBehaviour {
 
 	[SerializeField] TextMesh textMesh;
+	[SerializeField] bool hiraganaInput, kanjiInput;
 
 	//漢字変換後の入力を受け取るサンプルです。
 	//OnJPInput(string) は漢字変換を確定するたびにコールされ、
 	//確定した文字列を引数として取得できます。
 	void OnJPInput (string str) {
-		textMesh.text += str;
+		if (kanjiInput)
+			textMesh.text += str;
 	}
 
 	//漢字変換前のひらがなを受け取るサンプルです。
 	//OnJPCharInput(string) は文字入力のたびにコールされ、
 	//入力した文字列を引数として取得できます。
 	void OnJPCharInput (string str) {
-//		Debug.Log (str);
-		textMesh.text += str;
+		if (hiraganaInput)
+			textMesh.text += str;
 	}
 
 	//後ろの文字を消去します。仮。デリゲートでコールバックに変更する予定。
