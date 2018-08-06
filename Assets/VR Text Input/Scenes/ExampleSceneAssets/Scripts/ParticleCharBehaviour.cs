1ee4cbfe0c06f5ce08df28f2cd115bc1b964311e
yutoVR <yuto.fushimi@icloud.com>
2018-08-07 00:19:07 +0900

    Added random text backgrounds.

---
+++ Assets/VR Text Input/Scenes/ExampleSceneAssets/Scripts/ParticleCharBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ParticleCharBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+        //カメラのほうを向く
+        var diff = transform.position - Camera.main.transform.position;
+        transform.LookAt(transform.position + diff);
+    }
+}
