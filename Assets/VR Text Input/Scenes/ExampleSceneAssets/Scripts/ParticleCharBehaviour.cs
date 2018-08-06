18d00c4f66aac58d12e35b796b9ff59d76fc847e
yutoVR <yuto.fushimi@icloud.com>
2018-08-07 00:23:04 +0900

    Randomize background text size.

--- Assets/VR Text Input/Scenes/ExampleSceneAssets/Scripts/ParticleCharBehaviour.cs
+++ Assets/VR Text Input/Scenes/ExampleSceneAssets/Scripts/ParticleCharBehaviour.cs
@@ -7,5 +7,8 @@ public class ParticleCharBehaviour : MonoBehaviour
         //カメラのほうを向く
         var diff = transform.position - Camera.main.transform.position;
         transform.LookAt(transform.position + diff);
+
+        //背景テキストのサイズをランダム化
+        GetComponent<TextMesh>().characterSize = Random.Range(0.01f, 0.04f);
     }
 }
